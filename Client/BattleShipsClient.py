2e65eb2fba0307aa6379ae51a16fde58f2e1ccd4
marko <markotaht@gmail.com>
2016-12-02 20:18:14 +0200

    Algeline RPC vaja täsemalt uudistada võimalusi.

--- Client/BattleShipsClient.py
+++ Client/BattleShipsClient.py
@@ -1,10 +1,13 @@
 import pika
 import uuid
+import threading
 
 from commons import *
 
 class BattleShipsClient(object):
     def __init__(self):
+        self.asynclistener = threading.Thread(target=self.listenForUpdates)
+        self.asynclistener.start()
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(
                 host='localhost'))
 
@@ -16,24 +19,43 @@ class BattleShipsClient(object):
         self.channel.basic_consume(self.on_response, no_ack=True,
                                    queue=self.callback_queue)
 
+    #Stuff for asynccalls
+    def listenForUpdates(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+        channel = connection.channel()
 
-    #Mõte RPC jaoks
-    def bomb(self,x,y):
-        data = "%s:%d,%d"%(BOMBING,x,y)
-        response = self.call(data)
-        #HIT or MISS
-        return response
+        channel.exchange_declare(exchange='logs',
+                                 type='fanout')
+
+        result = channel.queue_declare(exclusive=True)
+        queue_name = result.method.queue
+
+        channel.queue_bind(exchange='updates',
+                           queue=queue_name)
+
+        print(' [*] Waiting for updates. To exit press CTRL+C')
+
+        def callback(ch, method, properties, body):
+            print(" [x] %r" % body)
+
+        channel.basic_consume(callback,
+                              queue=queue_name,
+                              no_ack=True)
+
+        channel.start_consuming()
 
     #Stuff for RPC/MQ
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def call(self, n):
+    def bomb(self, x,y,player):
+        n = str(x)+ ":" + str(y) + ":" + str(player)
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
+                                   routing_key='rpc_bomb',
                                    properties=pika.BasicProperties(
                                          reply_to = self.callback_queue,
                                          correlation_id = self.corr_id,
@@ -43,8 +65,24 @@ class BattleShipsClient(object):
             self.connection.process_data_events()
         return self.response
 
+    def placeShip(self,x,y,orientation):
+        n = str(x) + ":" + str(y) + ":" + str(orientation)
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_place_ship',
+                                   properties=pika.BasicProperties(
+                                       reply_to=self.callback_queue,
+                                       correlation_id=self.corr_id,
+                                   ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
 fibonacci_rpc = BattleShipsClient()
 
 print(" [x] Requesting fib(30)")
-response = fibonacci_rpc.call(30)
+response = fibonacci_rpc.placeShip(0,0,4)
+print(" [.] Got %r" % response)
+response = fibonacci_rpc.bomb(0,0,1)
 print(" [.] Got %r" % response)
--- Server/server.py
+++ Server/server.py
@@ -1,36 +1,60 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+class BattleShipsServer():
+    def __init__(self):
+        self.initChannels()
 
-channel = connection.channel()
+    def initChannels(self):
+        self.updateConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.updateChannel = self.updateConnection.channel()
+        self.updateChannel.exchange_declare(exchange='updates',type='fanout')
 
-channel.queue_declare(queue='rpc_queue')
+        self.placeShipConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.placeShipChannel = self.placeShipConnection.channel()
+        self.placeShipChannel.queue_declare(queue='rpc_place_ship')
 
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+        self.placeShipChannel.basic_qos(prefetch_count=1)
+        self.placeShipChannel.basic_consume(self.placeShip, queue='rpc_place_ship')
+        self.placeShipChannel.start_consuming()
 
-def on_request(ch, method, props, body):
-    n = int(body)
+        self.bombShipConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.bombShipChannel = self.placeShipConnection.channel()
+        self.bombShipChannel.queue_declare(queue='rpc_bomb')
 
-    print(" [.] fib(%s)" % n)
-    response = fib(n)
+        self.bombShipChannel.basic_qos(prefetch_count=1)
+        self.bombShipChannel.basic_consume(self.bombShip, queue='rpc_bomb')
+        self.bombShipChannel.start_consuming()
 
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
-                     body=str(response))
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    def bombShip(self, ch, method, props, body):
+        n = body
 
+        print(" [.] fib(%s)" % n)
+        response = "MISS"
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id= \
+                                                             props.correlation_id),
+                         body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
+        self.updateChannel.basic_publish(exchange='updates',
+                                         routing_key='',
+                                         body="KABOOM")
 
-print(" [x] Awaiting RPC requests")
-channel.start_consuming()
+    def placeShip(self, ch, method, props, body):
+        n = body
+
+        print(" [.] fib(%s)" % n)
+        response = "HIT"
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id= \
+                                                             props.correlation_id),
+                         body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        self.updateChannel.basic_publish(exchange='updates',
+                                         routing_key='',
+                                         body="WOLOLOLO")
+
+client = BattleShipsServer()
