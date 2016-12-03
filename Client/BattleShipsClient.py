0fe65f7ff82fb504e78030a105933f6f2f20a4e6
marko <markotaht@gmail.com>
2016-12-03 18:15:51 +0200

    Trying to create some kind of session/room system.

--- Client/BattleShipsClient.py
+++ Client/BattleShipsClient.py
@@ -8,16 +8,26 @@ class BattleShipsClient(object):
     def __init__(self):
         self.asynclistener = threading.Thread(target=self.listenForUpdates)
         self.asynclistener.start()
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+        self.placeShipConnecion = pika.BlockingConnection(pika.ConnectionParameters(
                 host='localhost'))
 
-        self.channel = self.connection.channel()
+        self.placeShipChannel = self.placeShipConnecion.channel()
 
-        result = self.channel.queue_declare(exclusive=True)
+        result = self.placeShipChannel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
 
-        self.channel.basic_consume(self.on_response, no_ack=True,
+        self.placeShipChannel.basic_consume(self.on_response, no_ack=True,
                                    queue=self.callback_queue)
+        self.bombShipConnection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+
+        self.bombShipChannel = self.bombShipConnection.channel()
+
+        result = self.bombShipChannel.queue_declare(exclusive=True)
+        self.callback_queue2 = result.method.queue
+
+        self.bombShipChannel.basic_consume(self.on_response2, no_ack=True,
+                                   queue=self.callback_queue2)
 
     #Stuff for asynccalls
     def listenForUpdates(self):
@@ -25,7 +35,7 @@ class BattleShipsClient(object):
             host='localhost'))
         channel = connection.channel()
 
-        channel.exchange_declare(exchange='logs',
+        channel.exchange_declare(exchange='updates',
                                  type='fanout')
 
         result = channel.queue_declare(exclusive=True)
@@ -50,26 +60,31 @@ class BattleShipsClient(object):
         if self.corr_id == props.correlation_id:
             self.response = body
 
+    def on_response2(self, ch, method, props, body):
+        if self.corr_id2 == props.correlation_id:
+            self.response2 = body
+
+
     def bomb(self, x,y,player):
         n = str(x)+ ":" + str(y) + ":" + str(player)
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
+        self.response2 = None
+        self.corr_id2 = str(uuid.uuid4())
+        self.bombShipChannel.basic_publish(exchange='',
                                    routing_key='rpc_bomb',
                                    properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
+                                         reply_to = self.callback_queue2,
+                                         correlation_id = self.corr_id2,
                                          ),
                                    body=str(n))
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
+        while self.response2 is None:
+            self.bombShipConnection.process_data_events()
+        return self.response2
 
     def placeShip(self,x,y,orientation):
         n = str(x) + ":" + str(y) + ":" + str(orientation)
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
+        self.placeShipChannel.basic_publish(exchange='',
                                    routing_key='rpc_place_ship',
                                    properties=pika.BasicProperties(
                                        reply_to=self.callback_queue,
@@ -77,12 +92,13 @@ class BattleShipsClient(object):
                                    ),
                                    body=str(n))
         while self.response is None:
-            self.connection.process_data_events()
+            self.placeShipConnecion.process_data_events()
         return self.response
 fibonacci_rpc = BattleShipsClient()
 
-print(" [x] Requesting fib(30)")
+print(" [x] Requesting place")
 response = fibonacci_rpc.placeShip(0,0,4)
 print(" [.] Got %r" % response)
+print(" [x] Requesting bomb")
 response = fibonacci_rpc.bomb(0,0,1)
 print(" [.] Got %r" % response)
--- Server/server.py
+++ Server/server.py
@@ -1,34 +1,46 @@
 import pika
+import threading
 
-class BattleShipsServer():
-    def __init__(self):
+class BattleShipsSession():
+    def __init__(self, server, name):
         self.initChannels()
+        self.server = server
+        self.name = name
+        self.prefix = server +"." + name + "."
 
     def initChannels(self):
         self.updateConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.updateChannel = self.updateConnection.channel()
-        self.updateChannel.exchange_declare(exchange='updates',type='fanout')
+        self.updateChannel.exchange_declare(exchange=self.prefix + 'updates',type='fanout')
 
+        self.placeship = threading.Thread(target=self.placeShipListener,args=(self.placeShip,))
+        self.bombship = threading.Thread(target=self.bombShipListener, args=(self.bombShip,))
+
+        self.placeship.start()
+        self.bombship.start()
+
+    def placeShipListener(self, placeShip):
         self.placeShipConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.placeShipChannel = self.placeShipConnection.channel()
         self.placeShipChannel.queue_declare(queue='rpc_place_ship')
 
         self.placeShipChannel.basic_qos(prefetch_count=1)
-        self.placeShipChannel.basic_consume(self.placeShip, queue='rpc_place_ship')
+        self.placeShipChannel.basic_consume(placeShip, queue='rpc_place_ship')
         self.placeShipChannel.start_consuming()
 
+    def bombShipListener(self, bombShip):
         self.bombShipConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.bombShipChannel = self.placeShipConnection.channel()
+        self.bombShipChannel = self.bombShipConnection.channel()
         self.bombShipChannel.queue_declare(queue='rpc_bomb')
 
         self.bombShipChannel.basic_qos(prefetch_count=1)
-        self.bombShipChannel.basic_consume(self.bombShip, queue='rpc_bomb')
+        self.bombShipChannel.basic_consume(bombShip, queue='rpc_bomb')
         self.bombShipChannel.start_consuming()
 
     def bombShip(self, ch, method, props, body):
         n = body
 
-        print(" [.] fib(%s)" % n)
+        print(" [.] bomb(%s)" % n)
         response = "MISS"
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
@@ -37,14 +49,14 @@ class BattleShipsServer():
                          body=str(response))
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-        self.updateChannel.basic_publish(exchange='updates',
+        self.updateChannel.basic_publish(exchange=self.prefix+'updates',
                                          routing_key='',
                                          body="KABOOM")
 
     def placeShip(self, ch, method, props, body):
         n = body
 
-        print(" [.] fib(%s)" % n)
+        print(" [.] place(%s)" % n)
         response = "HIT"
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
@@ -53,8 +65,8 @@ class BattleShipsServer():
                          body=str(response))
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-        self.updateChannel.basic_publish(exchange='updates',
+        self.updateChannel.basic_publish(exchange=self.prefix+'updates',
                                          routing_key='',
                                          body="WOLOLOLO")
 
-client = BattleShipsServer()
+client = BattleShipsSession("North WU", "Game1")
