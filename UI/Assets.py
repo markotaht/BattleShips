302bbc01972d41172019e04bd3dd053d5d2eb0bc
MadisKarli <matukoppel@gmail.com>
2016-12-06 14:45:30 +0200

    Moved fonts to assets

--- UI/Assets.py
+++ UI/Assets.py
@@ -8,3 +8,9 @@ COLOR_RED = (255, 0, 0)
 COLOR_GREEN = (0, 255, 0)
 COLOR_BLUE = (0, 0, 255)
 
+#fonts
+tinyFont = pygame.font.SysFont(None, 18)
+smallFont = pygame.font.SysFont(None, 24)
+mediumFont = pygame.font.SysFont(None, 36)
+largeFont = pygame.font.SysFont(None, 48)
+
